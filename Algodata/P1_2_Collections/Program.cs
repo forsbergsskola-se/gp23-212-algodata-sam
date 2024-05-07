5ae054e787dd74e8f8a4e4d7741220c56076f370
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-05-07 18:26:19 +0200

    Update P1_2_Collections

--- Algodata/P1_2_Collections/Program.cs
+++ Algodata/P1_2_Collections/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an ArrayList to store the values true, "Forsbergs", 'a', 1000, .12f
+        ArrayList arrayList = new ArrayList();
+        arrayList.Add(true);
+        arrayList.Add("Forsbergs");
+        arrayList.Add('a');
+        arrayList.Add(1000);
+        arrayList.Add(0.12f);
+
+        // Use a for-loop and the index-operator [] to print all values in the ArrayList
+        Console.WriteLine("\nValues in ArrayList:");
+        for (int i = 0; i < arrayList.Count; i++)
+        {
+            Console.WriteLine(arrayList[i]);
+        }
+    }
+}
