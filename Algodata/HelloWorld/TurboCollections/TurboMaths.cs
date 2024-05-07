66c7abe096ed90af0f39c3fa1ab8b613c6a522a0
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-05-07 13:20:27 +0200

    1-Collections TurboMaths lib

---
+++ Algodata/HelloWorld/TurboCollections/TurboMaths.cs
@@ -0,0 +1,10 @@
+namespace TurboCollections
+{
+    public static class TurboMaths
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine($"Hello, I'm {typeof(TurboMaths).FullName}");
+        }
+    }
+}
