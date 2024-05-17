9728cc34cad27ee97ddb821cc45c9dbf28945bf2
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-05-17 11:25:56 +0200

    Update UnitTest2.cs

--- Algodata/TurboCollections.Test/UnitTest2.cs
+++ Algodata/TurboCollections.Test/UnitTest2.cs
@@ -1,6 +1,28 @@
-namespace TurboCollections.Test;
+using NUnit.Framework;
+using System.Collections.Generic;
+using P1_3_Iterator.TurboCollections;
 
-public class UnitTest2
+namespace TurboCollections.Test
 {
-    
+    [TestFixture]
+    public class UnitTest2
+    {
+        [Test]
+        public void Test_GetEvenNumbersList()
+        {
+            Assert.That(TurboMaths2.GetEvenNumbersList(12), Is.EqualTo(new List<int> { 0, 2, 4, 6, 8, 10, 12 }));
+            Assert.That(TurboMaths2.GetEvenNumbersList(15), Is.EqualTo(new List<int> { 0, 2, 4, 6, 8, 10, 12, 14 }));
+            Assert.That(TurboMaths2.GetEvenNumbersList(-1), Is.EqualTo(new List<int>()));
+            Assert.That(TurboMaths2.GetEvenNumbersList(0), Is.EqualTo(new List<int> { 0 }));
+        }
+
+        [Test]
+        public void Test_GetEvenNumbers()
+        {
+            Assert.That(new List<int>(TurboMaths2.GetEvenNumbers(12)), Is.EqualTo(new List<int> { 0, 2, 4, 6, 8, 10, 12 }));
+            Assert.That(new List<int>(TurboMaths2.GetEvenNumbers(15)), Is.EqualTo(new List<int> { 0, 2, 4, 6, 8, 10, 12, 14 }));
+            Assert.That(new List<int>(TurboMaths2.GetEvenNumbers(-1)), Is.EqualTo(new List<int>()));
+            Assert.That(new List<int>(TurboMaths2.GetEvenNumbers(0)), Is.EqualTo(new List<int> { 0 }));
+        }
+    }
 }
