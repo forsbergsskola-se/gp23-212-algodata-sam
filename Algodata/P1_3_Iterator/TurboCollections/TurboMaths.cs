da6c89195ae1fca7ca06a12a395940923b6cbcf5
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-05-17 10:31:16 +0200

    Update TurboMaths.cs

--- Algodata/P1_3_Iterator/TurboCollections/TurboMaths.cs
+++ Algodata/P1_3_Iterator/TurboCollections/TurboMaths.cs
@@ -1,6 +1,25 @@
-namespace P1_3_Iterator.TurboCollections;
-
-public class TurboMaths
+namespace P1_3_Iterator
 {
-    
+    public static class TurboMaths
+    {
+        // Returns all Even Numbers until the given number.
+        public static List<int> GetEvenNumbersList(int maxNumber)
+        {
+            List<int> evenNumbers = new List<int>();
+            for (int i = 0; i <= maxNumber; i += 2)
+            {
+                evenNumbers.Add(i);
+            }
+            return evenNumbers;
+        }
+
+        // Returns all Even Numbers until the given number.
+        public static IEnumerable<int> GetEvenNumbers(int maxNumber)
+        {
+            for (int i = 0; i <= maxNumber; i += 2)
+            {
+                yield return i;
+            }
+        }
+    }
 }
